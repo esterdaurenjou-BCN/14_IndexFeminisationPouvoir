5e8094364667620d216e0de5a8c3aa8910acc626
20k-P <11522296+20k-P@users.noreply.github.com>
2026-03-22 19:53:06 +0100

    feat: Use TEXT instead of VARCHAR type

--- dbt_ifp/models/staging/stg_figure7a.sql
+++ dbt_ifp/models/staging/stg_figure7a.sql
@@ -1,18 +1,18 @@
 SELECT
-    CAST("__id" AS VARCHAR) AS id,
-    CAST("Code de la région" AS VARCHAR) AS code_region,
+    CAST("__id" AS TEXT) AS id,
+    CAST("Code de la région" AS TEXT) AS code_region,
     "Libellé de la région" AS libelle_region,
-    CAST("Code de la section départementale" AS VARCHAR) AS code_dep,
+    CAST("Code de la section départementale" AS TEXT) AS code_dep,
     "Libellé de la section départementale" AS libelle_dep,
     "Nom de l'élu" AS nom_elu,
     "Prénom de l'élu" AS prenom_elu,
     "Code sexe" AS code_sexe,
     "Date de naissance" AS date_naissance,
-    CAST("Code de la catégorie socio-professionnelle" AS VARCHAR) AS code_pcs,
+    CAST("Code de la catégorie socio-professionnelle" AS TEXT) AS code_pcs,
     "Libellé de la catégorie socio-professionnelle" AS libelle_pcs,
     "Date de début du mandat" AS date_mandat_debut,
     "Libellé de la fonction" AS libelle_fonction,
     "Date de début de la fonction" AS date_fonction_debut,
     source_url
 FROM
-        {{ ref('figure7a_20260320_140724') }}
+    {{ ref('figure7a_20260320_140724') }}
